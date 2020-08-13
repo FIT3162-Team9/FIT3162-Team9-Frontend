381a8898cc21547bfadfbe7a4dd2b7f69b159f7d
Jacky Eng <jacky.engcs@gmail.com>
2020-08-13 18:36:09 +1000

    feat(data visualisation): use Rechart to add a basic area chart for temperature visualisation

---
+++ src/components/Visualisation.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Chart from './Chart.js'
+
+
+function Visualisation(){
+    return(
+        <div>
+            <Chart/>
+        </div>
+    )
+}
+
+export default Visualisation
