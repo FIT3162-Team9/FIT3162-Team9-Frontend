1ad5178d330e67a9b27c04752517d24aa14faaaf
arnimittal <arnimittal@users.noreply.github.com>
2020-09-03 01:44:51 +1000

    select station id

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -10,32 +10,14 @@ function formatXAxis(tickItem) {
   return moment.unix(tickItem).format('YYYY-MM-DD')
 }
 
-
 export default ({ data }) => {
   
   return (
       <ResponsiveContainer height={300} width="95%">
-        {/* <AreaChart data={data} >
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="1%" stopColor="#FAD6A5" stopOpacity={0.8} />
-              <stop offset="99%" stopColor="#FAD6A5" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="1%" stopColor="#ABD1F3" stopOpacity={0.8} />
-              <stop offset="99%" stopColor="#ABD1F3" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="timestamp" tickFormatter={formatXAxis} />
-          <YAxis unit='°C' />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area type="monotone" dataKey="min" stroke="#FAD6A5" fillOpacity={1} fill="url(#colorUv)" />
-        </AreaChart> */}
         <LineChart data={data}>
           <XAxis dataKey="timestamp" tickFormatter={formatXAxis} />
           <YAxis unit='°C' />
-          <Tooltip />
+          <Tooltip/>
           <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
           <Line type="monotone" dataKey="max" stroke={'#8884d8'} dot={false}/>
           <Line type="monotone" dataKey="min" stroke={'#82ca9d'} dot={false}/>
--- src/components/Visualisation.js
+++ src/components/Visualisation.js
@@ -6,7 +6,13 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 import RefreshIcon from '@material-ui/icons/Refresh';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 import moment from 'moment';
 
 import Title from './Title';
@@ -15,7 +21,10 @@ import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 function Visualisation(){
     const [tempData, setTempData] = useState([]);
     const [dateRange, setDateRange] = useState([moment().subtract(13, 'months'), moment().subtract(1, 'month')]);
+    const [stationId, setStationId] = React.useState('76031');
 
+    const validStationIds = ['76031', '76047', '76064']
+    
     useEffect(() => {
         refreshTemp();
     }, [])
@@ -25,8 +34,12 @@ function Visualisation(){
         console.log('dateRange', formattedDateRange);
         const startTimestamp = formattedDateRange[0];
         const endTimestamp = formattedDateRange[1]
-        getTemperature('76031', setTempData, startTimestamp, endTimestamp);
+        getTemperature(stationId, setTempData, startTimestamp, endTimestamp);
     }
+
+    const handleStationIdChange = (event) => {
+        setStationId(event.target.value);
+    };
 
     return(
         <Grid xs={12} md={12} lg={12}>
@@ -38,15 +51,27 @@ function Visualisation(){
                 justify="space-between"
                 style={{paddingRight: 10, paddingLeft: 10, paddingTop: 10}}
             >
-                <Typography>Min Temperature</Typography>
-                <div>
-                    <IconButton aria-label="refresh" onClick={refreshTemp}>
-                        <RefreshIcon fontSize="small" />
-                    </IconButton>
+                <Typography>Temperature</Typography>
+                <div style={{justifyContent: 'center'}}>
+                   
+
+                    <FormControl size="small" style={{paddingRight: 10, paddingLeft: 10 }}>
+                        <Select
+                            labelId="station-id-label"
+                            id="station-id"
+                            value={stationId}
+                            onChange={handleStationIdChange}
+                        >
+                            {validStationIds.map(id => <MenuItem value={id}>{id}</MenuItem>)}
+                        </Select>
+                    </FormControl>
                     <DateRangePicker
                         onChange={setDateRange}
                         value={dateRange}
                     />
+                     <IconButton aria-label="refresh" onClick={refreshTemp}>
+                        <RefreshIcon fontSize="small" />
+                    </IconButton>
                 </div>
             </Grid>
             <Grid item xs={12} md={12} lg={12}>
