09ce2f9e25c3792ae13db3ba4390ebf4fa5c7cd6
arnimittal <arnimittal@users.noreply.github.com>
2020-10-18 16:59:18 +1100

    Move slider to bushfire

--- src/pages/BushfireAnalysis/index.js
+++ src/pages/BushfireAnalysis/index.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid'
 import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
-import CustomizedSlider from '../../components/Slider'
+import CustomizedSlider from './Slider'
 import { temperature as data } from '../../mocks/tempdata';
 import Analysis from './Analysis';
 import BushfireFilter from './BushfireFilter'
